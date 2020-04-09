1104c99e527b2ad213bec3be41bcd08bd19afd6b
suhailmohamed <suhailmohamed333@gmail.com>
2020-04-09 22:59:38 +0530

    Updates

---
+++ precache-manifest.7ef246342d4fff98ff459627313aab5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4d95110f9b8c0dd46c4",
+    "url": "/dashboard/static/css/main.38d3b6f1.chunk.css"
+  },
+  {
+    "revision": "e4d95110f9b8c0dd46c4",
+    "url": "/dashboard/static/js/main.e4d95110.chunk.js"
+  },
+  {
+    "revision": "dcfbc05562333eb34d98",
+    "url": "/dashboard/static/js/runtime~main.dcfbc055.js"
+  },
+  {
+    "revision": "d0bb4d3ff1b83bd5d37f",
+    "url": "/dashboard/static/js/2.d0bb4d3f.chunk.js"
+  },
+  {
+    "revision": "15196eb1799f84360752ef27c07e1718",
+    "url": "/dashboard/index.html"
+  }
+];
